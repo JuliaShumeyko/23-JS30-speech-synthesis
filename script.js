623a47eb5ba79d55d5108ac6f65ab61690443c51
Julia Shumeyko <46918344+JuliaShumeyko@users.noreply.github.com>
2019-10-09 22:30:28 +0300

    added voice options

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const msg = new SpeechSynthesisUtterance();
+const msg = new SpeechSynthesisUtterance(); // info on how fast something is said, in which voice, what is being said etc.
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').value;
+
+// Populating voices
+
+function populateVoices() {
+    voices = this.getVoices();
+    const voiceOptions = voices
+        .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+        .join('');
+    voicesDropdown.innerHTML = voiceOptions;
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices)
